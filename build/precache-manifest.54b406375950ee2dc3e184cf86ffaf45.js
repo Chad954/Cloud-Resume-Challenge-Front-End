3c1b91ff24a7dc338c0ab8f9f3cc6b1d02fbc27d
Chad954 <101349242+Chad954@users.noreply.github.com>
2022-09-07 12:51:45 -0400

    add modified gitignore

---
+++ build/precache-manifest.54b406375950ee2dc3e184cf86ffaf45.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f86b2307a5db9413339",
+    "url": "/static/css/main.cad970f1.chunk.css"
+  },
+  {
+    "revision": "0f86b2307a5db9413339",
+    "url": "/static/js/main.0f86b230.chunk.js"
+  },
+  {
+    "revision": "2602f24af8d7c544ec5e",
+    "url": "/static/js/1.2602f24a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7179ca7b7075e1f11123c6edb2e39fa8",
+    "url": "/static/media/resume2.7179ca7b.PDF"
+  },
+  {
+    "revision": "16c9ba555e118668400aae445f6792e2",
+    "url": "/static/media/chadddd.16c9ba55.png"
+  },
+  {
+    "revision": "69021d5591217584aaab85a463557406",
+    "url": "/static/media/chad.69021d55.JPG"
+  },
+  {
+    "revision": "8478339793ca5f6d06f6e258b6a33248",
+    "url": "/static/media/resumepic.84783397.jpg"
+  },
+  {
+    "revision": "a4130b659515099994f1c4502177a26a",
+    "url": "/index.html"
+  }
+];
